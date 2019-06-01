cb4b8d365168f785700aa63f74eaaf1b5759df6a
germweapon <ysk08900@gmail.com>
2019-06-01 23:42:50 +0900

    work in porgress

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,29 +1,28 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: ``, // Your blog site url
+  title: `세균무기, 지구별에 흔적을 남기다!`,
+  description: `IT서비스 기획자이자 프로덕트 매니저인 세균무기가 지구별에 남긴 흔적들.`,
+  author: `germweapon`,
+  introduction: `안녕하세요! '세균무기, 지구별에 흔적을 남기다!' 블로그를 운영하는 세균무기(germweapon)입니다.<br/>세균무기는 IT서비스 기획자이자 프로덕트 매니저입니다. SF영화를 보면서 공상하고 영화 주인공들에 자신을 투영하며 스스로를 반성하는 것을 좋아하는 몽상가이자 완벽주의자입니다. 새로운 서비스를 보고 경험하며 세상이 빠르게 변화하는 것에 열광하지만 법학과 행정학을 전공하고 철학과 가치를 소중하게 생각하는 머리는 차갑고 가슴은 따뜻한 얼리어답터이자 이상주의자입니다. 꿈과 목표는 열정 있는 동료들과 함께 재미있게 일하고, 가치있는 서비스를 기획하여 사회에 이바지하며, 그 성공을 바탕으로 가슴 따뜻한 세상을 만드는데 조금이나마 기여하는 것입니다.`,
+  siteUrl: `https://germweapon.tistory.com/`,
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    twitter: `https://twitter.com/germweapon`,
+    facebook: `https://www.facebook.com/germweapon`,
+    github: `https://github.com/germweapon`,
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/profile.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'germweapon/gatsby-starter-bee', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'germweapon',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-17272968-3', // Add your google analytics tranking ID
 }
